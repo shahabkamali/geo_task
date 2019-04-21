341f9cd9a869ab1e5320b4136d7ce840b4ea9fac
shahab <shahab kamali>
2019-04-22 02:41:51 +0430

    adding docker file and config

---
+++ src/geoapp/views.py
@@ -0,0 +1,60 @@
+from rest_framework import decorators
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.gis.geos import Point
+
+
+from utilities.tools import create, delete, list_result, update
+from utilities.utilities import CUSTOM_PAGINATION_SCHEMA
+from utilities import permissions
+from geoapp.models import Geo
+from geoapp.serializers import GeoSerializer
+
+
+@decorators.authentication_classes([JSONWebTokenAuthentication])
+@decorators.permission_classes([IsAuthenticated])
+class GeoCreateView(create.CreateView):
+    serializer_class = GeoSerializer
+    model = Geo
+
+
+@decorators.authentication_classes([JSONWebTokenAuthentication])
+@decorators.permission_classes([IsAuthenticated])
+@decorators.schema(CUSTOM_PAGINATION_SCHEMA)
+class GeoListView(list_result.ListView):
+    serializer_class = GeoSerializer
+    model = Geo
+
+
+@decorators.authentication_classes([JSONWebTokenAuthentication])
+@decorators.permission_classes([IsAuthenticated])
+class GeoUpdateView(update.UpdateView):
+    serializer_class = GeoSerializer
+    model = Geo
+
+
+@decorators.authentication_classes([JSONWebTokenAuthentication])
+@decorators.permission_classes([IsAuthenticated])
+class GeoDeleteView(delete.DeleteView):
+    serializer_class = GeoSerializer
+    model = Geo
+
+
+@decorators.authentication_classes([JSONWebTokenAuthentication])
+@decorators.permission_classes([IsAuthenticated])
+class PairQueryView(APIView):
+    model = Geo
+
+    def get(self, request, format=None):
+        """
+        return a list of all polygons that include
+        the given lat/lng
+        """
+        lat = float(request.query_params.get('lat', 0))
+        lng = float(request.query_params.get('lng', 0))
+        point = Point(lng, lat)
+        polygons = Geo.objects.filter(polygon__contains=point)
+        serializer = GeoSerializer(polygons, many=True)
+        return Response(serializer.data)
