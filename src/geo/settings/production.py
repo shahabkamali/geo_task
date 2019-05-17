5466a96ce56934879972c3aeff2e23e4cac17897
shahab <shahab kamali>
2019-05-17 19:10:49 +0430

    get pass of db from ENV

--- src/geo/settings/production.py
+++ src/geo/settings/production.py
@@ -8,6 +8,9 @@ ALLOWED_HOSTS = []
 
 SECRET_KEY = os.environ.get('SECRET_KEY', None)
 
+DB_PASS = os.environ.get('DB_PASS', None)
+
+
 if not SECRET_KEY:
     raise Exception('Add SECRET_KEY env variable, example: export SECRET_KEY=123456')
 
@@ -16,7 +19,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'geodb',
         'USER': 'geo',
-        'PASSWORD': 'Ge0dB',
+        'PASSWORD': DB_PASS,
         'HOST': 'localhost',
         'PORT': '',
     }
